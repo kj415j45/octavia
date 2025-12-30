3f5bdf495d2b590baf246e085ea998ed23c29c3d
kj415j45 <919815238@qq.com>
2025-12-30 17:43:10 +0800

    Fix enum?

--- src/apis/stage_info.ts
+++ src/apis/stage_info.ts
@@ -1,8 +1,9 @@
 import octavia, { Regions } from '../octavia';
 
 export async function getStageInfo(region: string, stageId: string) {
-	if (!(region in Regions)) {
-		throw new Error(`Invalid region: ${region}`);
-	}
+	const validRegions = Object.values(Regions);
+    if (!validRegions.includes(region as Regions)) {
+        throw new Error(`Invalid region: ${region}. Valid regions are: ${validRegions.join(', ')}`);
+    }
 	return octavia.getStageInfo(Regions[region as keyof typeof Regions], stageId);
 }
