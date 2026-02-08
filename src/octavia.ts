6e84281b7a4f203fddea6996f65809dd352b9586
kj415j45 <kj415j45@gmail.com>
2026-02-08 16:57:54 +0800

    Add support for Asia, Europe, America server.

--- src/octavia.ts
+++ src/octavia.ts
@@ -3,6 +3,12 @@ const defaultUA = 'Octavia/1.0.0 (kj415j45/octavia)';
 export enum Regions {
 	CN_GF = 'cn_gf01',
 	CN_BILI = 'cn_qd01',
+
+	GLB_AS = 'os_asia',
+	GLB_EU = 'os_euro',
+	GLB_NA = 'os_usa',
+
+	
 }
 
 class Octavia {
@@ -12,8 +18,22 @@ class Octavia {
 		this.userAgent = userAgent ?? defaultUA;
 	}
 
+	protected getEndpoint(region: Regions) {
+		switch (region) {
+			case Regions.CN_GF:
+			case Regions.CN_BILI:
+				return 'https://bbs-api.miyoushe.com/community/ugc_community/web/api/level/full/info';
+			case Regions.GLB_NA:
+			case Regions.GLB_EU:
+			case Regions.GLB_AS:
+				return 'https://bbs-api-os.hoyolab.com/community/ugc_community/web/api/level/full/info';
+			default:
+				throw new Error(`Unsupported region: ${region}`);
+		}
+	}
+
 	async getStageInfo(region: Regions, stageId: string) {
-		const endpoint = 'https://bbs-api.miyoushe.com/community/ugc_community/web/api/level/full/info';
+		const endpoint = this.getEndpoint(region);
 		const payload = {
 			region,
 			level_id: stageId,
@@ -24,7 +44,7 @@ class Octavia {
 		console.debug('Received stage info:', resp_map);
 		const levelDetail = resp_map.level_detail.data.level_detail_response.level_info;
 		const developerInfo = resp_map.developer_info.data.developer_news_response;
-		const defaultAvatar = 'https://bbs-static.miyoushe.com/upload/op_manual_upload/ugc_community/1769653604473developer_default_avatar.png';
+
 		const level = {
 			region: region,
 			id: stageId,
@@ -33,7 +53,7 @@ class Octavia {
 				description: levelDetail.desc,
 				intro: levelDetail.level_intro,
 				type: levelDetail.play_type,
-				category: levelDetail.play_cate === 'LEVEL_CATE_LONG_TERM' ? '长线游玩' : '轻量趣味',
+				category: this.parseStageCategory(levelDetail.play_cate),
 				tags: levelDetail.play_tags,
 				players: {
 					min: levelDetail.limit_play_num_min,
@@ -55,21 +75,48 @@ class Octavia {
 			},
 		};
 		const mysInfo = developerInfo.developer.mys_user_info;
+		const hylInfo = developerInfo.developer.hyl_user_info;
 		const author = {
 			game: {
-				avatar: developerInfo.developer.game_avatar === defaultAvatar ? null : developerInfo.developer.game_avatar,
+				avatar: this.getValidAvatar(developerInfo.developer.game_avatar),
 				name: developerInfo.developer.game_nickname,
 			},
 			mys: {
 				aid: developerInfo.developer.aid,
-				avatar: mysInfo?.avatar_url == "" ? null : mysInfo?.avatar_url,
+				avatar: this.getValidAvatar(mysInfo?.avatar_url),
 				name: mysInfo?.nickname,
 			},
+			hyl: {
+				aid: developerInfo.developer.aid,
+				avatar: this.getValidAvatar(hylInfo?.avatar_url),
+				pedant: hylInfo?.pedant,
+				name: hylInfo?.nickname,
+			}
 		};
 		return {
 			author,
 			level,
 		};
+	}
+
+	protected getValidAvatar(avatar: string) {
+		const mysDefaultAvatar = 'https://bbs-static.miyoushe.com/upload/op_manual_upload/ugc_community/1769653604473developer_default_avatar.png';
+		const hylDefaultAvatar = 'https://fastcdn.hoyoverse.com/static-resource-v2/2026/02/03/d82e8dcf18900bc983428acc1e4961d2_279538261314927397.png';
+		if (!avatar || avatar === mysDefaultAvatar || avatar === hylDefaultAvatar) {
+			return null;
+		}
+		return avatar;
+	}
+
+	protected parseStageCategory(category: string) {
+		switch (category) {
+			case 'LEVEL_CATE_LONG_TERM':
+				return '长线游玩';
+			case 'LEVEL_CATE_QUICK_EXPERIENCE':
+				return '轻量趣味';
+			default:
+				return category;
+		}
 	}
 
 	protected async request(method: string, endpoint: string, query: Record<string, any>, payload: any): Promise<any> {
